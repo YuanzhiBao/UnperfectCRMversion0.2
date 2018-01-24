e97cc8be0a31b738c3d32bdc4cf61fc8052ce1ff
YuanzhiBao <baoyu@indiana.edu>
2018-01-23 19:25:15 -0500

    Filter List including datefield show on the frontend with no filter functions
    筛选框包括datefield都出来了，但是没有过滤功能

--- kingadmin/templatetags/kingadmin_tags.py
+++ kingadmin/templatetags/kingadmin_tags.py
@@ -6,6 +6,8 @@ from django.template import Library
 
 from django.utils.safestring import mark_safe
 
+import datetime,time
+
 register = Library()
 
 
@@ -28,16 +30,39 @@ def build_table_row(obj, admin_class):
 def build_filter_row(filter_obj, admin_class):
     opt = ""
     # print(filter_list)
-    print(filter_obj)
+    # print(filter_obj)
     field_name = admin_class.model._meta.get_field(filter_obj)
     try:
         opt += "<select>"
         for choice in field_name.get_choices():
-            opt += "<option>%s - %s</option>" % choice
-            print(choice)
+            opt += "<option %s>%s</option>" % choice
+            # print(choice)
         opt += "</select>"
     except AttributeError:
-        pass
+        type_of_internal = field_name.get_internal_type()
+        # print(type_of_internal)
+        if type_of_internal in ('DateField','DateTimefield'):
+            now_time = datetime.datetime.now()
+            time_opt_list = [
+                ('', "-------"),
+                ( now_time,"Today"),
+                ( now_time - datetime.timedelta(7),"7 Days Ago"),
+                ( now_time.replace(day=1),"This month"),
+                ( now_time - datetime.timedelta(90),"Within Three month"),
+                ( now_time.replace(month=1,day=1),"This Year"),
+            ]
+            for time_slot in time_opt_list:
+                print(type(time_slot[1]))
+                if time_slot[0] != "":
+                    year = str(time_slot[0].year)
+                    month = str(time_slot[0].month)
+                    day = str(time_slot[0].day)
+                    opt += "<option %s-%s-%s>%s</option>" \
+                           % (time_slot[0].year, time_slot[0].month, time_slot[0].day, time_slot[1])
+                    # print(choice)
+                else:
+                    opt += "<option >------</option>"
+            opt += "</select>"
     # print(opt)
 
     return mark_safe(opt)
